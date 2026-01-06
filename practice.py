563c0df73dae57360b1d0b26dab89f83bc11adb5
Dikshit Phuyal <dikshit.phuyals24@cps.edu.np>
2026-01-06 22:02:20 +0545

    added database system

--- practice.py
+++ practice.py
@@ -1,27 +1,82 @@
-from flask import Flask, request
-from flask_restful import Api, Resource
-from flask_jwt_extended import JWTManager, create_access_token
+from flask import Flask, request, redirect
+from flask.templating import render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-api = Api(app)
+app.debug = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Avoids a warning
 
-app.config["JWT_SECRET_KEY"] = "my-key"
-jwt = JWTManager(app)
+db = SQLAlchemy(app)
 
+# Model
+class Profile(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(20), unique=False, nullable=False)
+    last_name = db.Column(db.String(20), unique=False, nullable=False)
+    age = db.Column(db.Integer, nullable=False)
 
-class Login(Resource):
-    def post(self):
-        data = request.get_json()
+    # repr method represents how one object of this datatable
+    # will look like
+    def __repr__(self):
+        return f"Name : {self.first_name}, Age: {self.age}"
 
-        if data["username"] == "admin" and data["password"] == "2468":
-            token = create_access_token(identity=data["username"])
-            return {"access_token": token}, 200
+from flask import Flask, request, redirect
+from flask.templating import render_template
+from flask_sqlalchemy import SQLAlchemy
 
-        return {"msg": "invalid credentials"}, 401
+app = Flask(__name__)
+app.debug = True
 
+# adding configuration for using a sqlite database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
-api.add_resource(Login, "/login")
+# Creating an SQLAlchemy instance
+db = SQLAlchemy(app)
 
+# Models
+class Profile(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(20), unique=False, nullable=False)
+    last_name = db.Column(db.String(20), unique=False, nullable=False)
+    age = db.Column(db.Integer, nullable=False)
 
-if __name__ == "__main__":
+    def __repr__(self):
+        return f"Name : {self.first_name}, Age: {self.age}"
+
+# function to render index page
+@app.route('/')
+def index():
+    profiles = Profile.query.all()
+    return render_template('index.html', profiles=profiles)
+
+@app.route('/add_data')
+def add_data():
+    return render_template('add_profile.html')
+
+# function to add profiles
+@app.route('/add', methods=["POST"])
+def profile():
+    first_name = request.form.get("first_name")
+    last_name = request.form.get("last_name")
+    age = request.form.get("age")
+
+    if first_name != '' and last_name != '' and age is not None:
+        p = Profile(first_name=first_name, last_name=last_name, age=age)
+        db.session.add(p)
+        db.session.commit()
+        return redirect('/')
+    else:
+        return redirect('/')
+
+@app.route('/delete/<int:id>')
+def erase(id): 
+    data = Profile.query.get(id)
+    db.session.delete(data)
+    db.session.commit()
+    return redirect('/')
+
+if __name__ == '__main__':
+    with app.app_context():  # Needed for DB operations outside a request
+        db.create_all()      # Creates the database and tables
     app.run(debug=True)

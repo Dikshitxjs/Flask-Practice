b09a89fdf6f66449387450f1a1286fbbaeb92721
Dikshit Phuyal <dikshit.phuyals24@cps.edu.np>
2026-01-13 18:53:19 +0545

    HTML escaping

---
+++ practice3.py
@@ -0,0 +1,11 @@
+from flask import Flask, request
+from markupsafe import escape
+
+app = Flask(__name__)
+
+app.route("/hello")
+def hello():
+    name = request.args.get("name", "Flask")
+    return f"Hello, {escape(name)}!"
+if __name__ == "__main__":
+    app.run(debug=True)
